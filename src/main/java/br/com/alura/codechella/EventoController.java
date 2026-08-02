cfb3357f60618791e89c84a7eb4c705c281802be
fmacedosantos <felipemacedo986@gmail.com>
2026-08-02 14:56:08 -0300

    obtém evento por id

--- src/main/java/br/com/alura/codechella/EventoController.java
+++ src/main/java/br/com/alura/codechella/EventoController.java
@@ -2,9 +2,11 @@ package br.com.alura.codechella;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
@@ -18,5 +20,10 @@ public class EventoController {
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoDto> obterTodos() {
         return servico.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
     }
 }
--- src/main/java/br/com/alura/codechella/EventoService.java
+++ src/main/java/br/com/alura/codechella/EventoService.java
@@ -1,7 +1,10 @@
 package br.com.alura.codechella;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -13,6 +16,12 @@ public class EventoService {
 
     public Flux<EventoDto> obterTodos() {
         return repositorio.findAll()
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return repositorio.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .map(EventoDto::toDto);
     }
 }
